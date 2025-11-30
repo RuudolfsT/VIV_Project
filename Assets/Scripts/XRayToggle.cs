116cdc5cee94fc1bcb6ada2af9316d99f2bbf52c
RuudolfsT <rudolfs.arvids@gmail.com>
2025-11-30 15:29:19 +0200

    Key import, script fixes

--- Assets/Scripts/XRayToggle.cs
+++ Assets/Scripts/XRayToggle.cs
@@ -6,17 +6,19 @@ public class XRayKeyToggle : MonoBehaviour
 {
     public Key xrayKey = Key.X;
     public ScriptableRendererFeature objectHiddenFeature;
+    public ScriptableRendererFeature aiHiddenFeature;
 
     void Update()
     {
         if (Keyboard.current[xrayKey].isPressed)
         {
             objectHiddenFeature.SetActive(true);
-
+            aiHiddenFeature.SetActive(true);
         }
         else
         {
             objectHiddenFeature.SetActive(false);
+            aiHiddenFeature.SetActive(false);
         }
     }
 }
